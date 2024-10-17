beb1e5b7a8ccb412ea805f9f0f997e97bad9f438
Clément Léger <cleger@rivosinc.com>
2024-10-17 14:25:00 +0200

    target/riscv: Fix henvcfg potentially containing stale bits
    With the current implementation, if we had the following scenario:
    - Set bit x in menvcfg
    - Set bit x in henvcfg
    - Clear bit x in menvcfg
    then, the internal variable env->henvcfg would still contain bit x due
    to both a wrong menvcfg mask used in write_henvcfg() as well as a
    missing update of henvcfg upon menvcfg update.
    This can lead to some wrong interpretation of the context. In order to
    update henvcfg upon menvcfg writing, call write_henvcfg() after writing
    menvcfg. Clearing henvcfg upon writing the new value is also needed in
    write_henvcfg() as well as clearing henvcfg upper part when writing it
    with write_henvcfgh().
    
    Signed-off-by: Clément Léger <cleger@rivosinc.com>
    Reviewed-by: Daniel Henrique Barboza <dbarboza@ventanamicro.com>
    Reviewed-by: Alistair Francis <alistair.francis@wdc.com>

--- target/riscv/csr.c
+++ target/riscv/csr.c
@@ -2375,6 +2375,8 @@ static RISCVException read_menvcfg(CPURISCVState *env, int csrno,
     return RISCV_EXCP_NONE;
 }
 
+static RISCVException write_henvcfg(CPURISCVState *env, int csrno,
+                                    target_ulong val);
 static RISCVException write_menvcfg(CPURISCVState *env, int csrno,
                                     target_ulong val)
 {
@@ -2401,6 +2403,7 @@ static RISCVException write_menvcfg(CPURISCVState *env, int csrno,
         }
     }
     env->menvcfg = (env->menvcfg & ~mask) | (val & mask);
+    write_henvcfg(env, CSR_HENVCFG, env->henvcfg);
 
     return RISCV_EXCP_NONE;
 }
@@ -2412,6 +2415,8 @@ static RISCVException read_menvcfgh(CPURISCVState *env, int csrno,
     return RISCV_EXCP_NONE;
 }
 
+static RISCVException write_henvcfgh(CPURISCVState *env, int csrno,
+                                    target_ulong val);
 static RISCVException write_menvcfgh(CPURISCVState *env, int csrno,
                                      target_ulong val)
 {
@@ -2422,6 +2427,7 @@ static RISCVException write_menvcfgh(CPURISCVState *env, int csrno,
     uint64_t valh = (uint64_t)val << 32;
 
     env->menvcfg = (env->menvcfg & ~mask) | (valh & mask);
+    write_henvcfgh(env, CSR_HENVCFGH, env->henvcfg >> 32);
 
     return RISCV_EXCP_NONE;
 }
@@ -2527,7 +2533,7 @@ static RISCVException write_henvcfg(CPURISCVState *env, int csrno,
         }
     }
 
-    env->henvcfg = (env->henvcfg & ~mask) | (val & mask);
+    env->henvcfg = val & mask;
 
     return RISCV_EXCP_NONE;
 }
@@ -2560,7 +2566,7 @@ static RISCVException write_henvcfgh(CPURISCVState *env, int csrno,
         return ret;
     }
 
-    env->henvcfg = (env->henvcfg & ~mask) | (valh & mask);
+    env->henvcfg = (env->henvcfg & 0xFFFFFFFF) | (valh & mask);
     return RISCV_EXCP_NONE;
 }
 
